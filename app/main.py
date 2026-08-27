0d8917454dff9dd7826435dce4edeaa4c8145ea3
EYOB <eyobzawude76@gmail.com>
2026-08-27 19:41:40 +0300

    MM

--- app/main.py
+++ app/main.py
@@ -46,7 +46,7 @@ app.add_middleware(AuditMiddleware)
 app.add_middleware(RequestLoggerMiddleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
